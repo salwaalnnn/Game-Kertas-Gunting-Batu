3ec507ad36f8d8e44245afe6e7f268645a71ebad
salwaalnnn <salwaalnnn@users.noreply.github.com>
2022-06-26 14:01:49 +0700

    Update Tugas Besar.py

--- Tugas Besar.py
+++ Tugas Besar.py
@@ -1,13 +1,10 @@
-# untuk memanggil semua komponen yang ada pada tkinter agar bisa kita gunakan untuk membuat program
-# tanda bintang menunjukan all atau semua artinya import semua yang ada di modul tkinter
 from tkinter import *
 from random import randint
-#buat jendela utam
+#buat jendela utama
 main_window = Tk()
 main_window.title("Permainan Kertas Gunting Batu Asik")
 # tempat untuk menyimpan program kita
 main_window.iconbitmap("C:\\Users\\LENOVO\\Coding\\ALPRO TUGAS")
-main_window.geometry("1050x400")
 main_window.config(background="pink")
 
 gunting = PhotoImage(file="C:\\Users\\LENOVO\\Coding\\ALPRO TUGAS\\Gunting.png")
@@ -15,17 +12,25 @@ batu = PhotoImage(file="C:\\Users\\LENOVO\\Coding\\ALPRO TUGAS\\Batu.png")
 kertas = PhotoImage(file="C:\\Users\\LENOVO\\Coding\\ALPRO TUGAS\\Kertas.png")
 kbg = PhotoImage(file="C:\\Users\\LENOVO\\Coding\\ALPRO TUGAS\\kbt.png")
 
-#menampilkan gambar ketika mulai
-labelpemain = Label(main_window,image=kbg)
-labelkomputer = Label(main_window,image=kbg)
-labelpemain.grid(row=1, column=0)
-labelkomputer.grid(row=1, column=4)
+# Menampilkan tampilan utama
+Judultampilan = Label(main_window, text="Suit Kertas Gunting Batu",font=("algerian",35,"bold"),background="light green", foreground= "black")
+Judultampilan.grid(row=0, column=2)
+spasi = Label(main_window, text=" ",font=("arial",30,"bold"),background="pink")
+spasi.grid(row=1, column=2)
+spemain = Label(main_window, text=0,font=("arial",60,"bold"),background="pink",foreground= "black")
+spemain.grid(row=3, column=1)
+skomputer = Label(main_window, text=0,font=("arial",60,"bold"),background="pink",foreground= "black")
+skomputer.grid(row=3, column=3)
+pemain = Label(main_window, font=("snap itc",30), text="PEMAIN" ,background="light green", foreground= "black")
+pemain.grid(row=2, column=1)
+komputer = Label(main_window, font=("snap itc",30), text="KOMPUTER" ,background="light green", foreground= "black")
+komputer.grid(row=2, column=3)
 
-# Menampilkan score, komputer dan pemain
-spemain = Label(main_window, text=0,font=("calibri",60,"bold"),background="light blue",foreground= "black").grid(row=1, column=1)
-skomputer = Label(main_window, text=0,font=("calibri",60,"bold"),background="light blue",foreground= "black").grid(row=1, column=3)
-pemain = Label(main_window, font=("snap itc",30), text="PEMAIN" ,background="light blue", foreground= "black").grid(row=0, column=1)
-komputer = Label(main_window, font=("snap itc",30), text="KOMPUTER" ,background="light blue", foreground= "black").grid(row=0, column=3)
+#menampilkan gambar ketika mulai
+gambarpemain = Label(main_window,image=kbg)
+gambarkomputer = Label(main_window,image=kbg)
+gambarpemain.grid(row=3, column=0)
+gambarkomputer.grid(row=3, column=4)
 
 # update pemberitahuan
 def pesanupdate(a):
@@ -43,27 +48,27 @@ def pemainupdate():
 # p = pemain k = komputer
 def cek_winner(p,k):
     if p == k :
-        pesanupdate("Yap Hasilnya seri")
+        pesanupdate("Hasilnya seri nih! Yuk coba lagi!")
     elif p == "batu":
         if k == "kertas":
-            pesanupdate("Yah Komputer menang Kamu kalah...")
+            pesanupdate("Yah Komputer menang Kamu kalah :(")
             komputerupdate()
         else:
-            pesanupdate("Yeay Kamu menang !!!")
+            pesanupdate("Yeay Kamu menang horeee !!!")
             pemainupdate()
     elif p == "gunting":
         if k == "batu":
-            pesanupdate("Yah Komputer menang Kamu kalah...")
+            pesanupdate("Yah Komputer menang Kamu kalah :(")
             komputerupdate()
         else:
-            pesanupdate("Yeay Kamu menang !!!")
+            pesanupdate("Yeay Kamu menang horeee !!!")
             pemainupdate()
     elif p == "kertas":
         if k == "gunting":
-            pesanupdate("Yah Komputer menang Kamu kalah...")
+            pesanupdate("Yah Komputer menang Kamu kalah :(")
             komputerupdate()
         else:
-            pesanupdate("Yeay Kamu menang !!!")
+            pesanupdate("Yeay Kamu menang horeee !!!")
             pemainupdate()
     else:
         pass
@@ -73,32 +78,33 @@ pilihkgb = ["kertas","gunting", "batu"]
 def pilihupdate(a):
     pilihkomputer = pilihkgb[randint(0,2)]
     if pilihkomputer == "kertas":
-        labelkomputer.config(image=kertas)
+        gambarkomputer.config(image=kertas)
     elif pilihkomputer == "gunting":
-        labelkomputer.config(image=gunting)
+        gambarkomputer.config(image=gunting)
     else:
-        labelkomputer.config(image=batu)
+        gambarkomputer.config(image=batu)
     # a untuk user seperti yang udah ada di atas
     if a == "kertas":
-        labelpemain.config(image=kertas)
+        gambarpemain.config(image=kertas)
     elif a == "gunting":
-        labelpemain.config(image=gunting)
+        gambarpemain.config(image=gunting)
     else:
-        labelpemain.config(image=batu)
+        gambarpemain.config(image=batu)
 
     #cek pemenang dengan panggil fungsi
     cek_winner(a,pilihkomputer)
 
 # menampilkan pemberitahuan
-pesanfinal =  Label(main_window,font=("calibri",40,"bold"),background="purple",foreground= "black").grid(row=4, column=2)
+pesanfinal =  Label(main_window,font=("calibri",20,"bold"),background="purple",foreground= "white")
+pesanfinal.grid(row=5, column=2)
 
 #tombol kertas gunting batu
 tbatu = Button(main_window, width= 14, height=3, text="Batu",
-                font=("calibri",18,"bold"), background="light blue", foreground= "black", command=lambda:pilihupdate("batu")).grid(row=3, column=1)
+                font=("calibri",18,"bold"), background="light blue", foreground= "black", command=lambda:pilihupdate("batu")).grid(row=4, column=1)
 tgunting = Button(main_window, width= 14, height=3, text="Gunting",
-                font=("calibri",18,"bold"), background="light blue", foreground= "black", command=lambda:pilihupdate("gunting")).grid(row=3, column=2)
+                font=("calibri",18,"bold"), background="light blue", foreground= "black", command=lambda:pilihupdate("gunting")).grid(row=4, column=2)
 tkertas = Button(main_window, width= 14, height=3, text="Kertas",
-                font=("calibri",18,"bold"), background="light blue", foreground= "black", command=lambda:pilihupdate("kertas")).grid(row=3, column=3)
+                font=("calibri",18,"bold"), background="light blue", foreground= "black", command=lambda:pilihupdate("kertas")).grid(row=4, column=3)
 
 # untuk menjalankan program yang sudah dibuat sampai program berakhir
 main_window.mainloop()
